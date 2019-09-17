30b56c8371dd3c0907cc22de75853190fe665bcf
batman <batman@gmail.com>
2019-09-17 18:06:37 +0530

    pop

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+    constructor(private authService: AuthenticationService) {}
 
-  ngOnInit() {
-  }
+    login() {
+      this.authService.getRequestToken().subscribe(res=>{
+        console.log(`Token Received ${res}`);
+        if (res && res.oauth_callback_confirmed){
+          this.authService.login(res.oauth_token);
+        }
+      }, err=>console.log(err));
+    }
 
 }
