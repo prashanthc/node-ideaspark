86bf982b3d1d17b4452d57a1c4f8cb9e29c65582
batman <batman@gmail.com>
2019-09-17 15:33:32 +0530

    Update

--- app.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    consumerKey: '4iP2yM12X6zBVeaHRxh0MsXOT',
-    consumerSecret: 'Jm96tJxGJJFecZkPiTwVlNpyWpMngJm6tRhxmATjFefckiEkOU',
-    connectionString: 'mongodb://localhost:27017/demo'
-};
--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,13 @@
 const config = require('../app.config.js');
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>console.log('Connection to DB Successful...'));
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('Connection to DB Successful...'));
+mongoose.connection.on('error', (error) => {
+    console.log('Db Connection error:', error);
+});
+
+mongoose.connection.once('open', function () {
+    console.log('Database connected');
+});
 mongoose.Promise = global.Promise;
 
 module.exports = {
